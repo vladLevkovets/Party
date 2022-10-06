79e5b341e0ffa2af5fafcc9f84f964737d5e4a2b
vladLevkovets <vl.vladisla@gmail.com>
2022-10-06 20:30:56 +0200

    home

--- client-side/App.js
+++ client-side/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, Button } from 'react-native';
 import { Audio } from 'expo-av';
 import React from "react"
-
+import Constants from 'expo-constants'
 
 
 
@@ -19,55 +19,187 @@ export default function App() {
   }
 
   React.useEffect(() => {
-    playSound()
+    // playSound()
   }, []);
 
   console.log("test")
   return (
-    
-    <View style={styles.container}>
-      
-      
-      <View style={styles.part1}><Text style={styles.text}>LETS START MYSELF</Text></View>
-      {/* <Button title="Play Sound" onPress={playSound} /> */}
-      <View style={styles.part2}><Text style={styles.text}>Open up App.js to start working on your app!</Text></View>
+     
+    <SafeAreaView style={styles.container}>
+      <View style={styles.TOP} >
+      <View style={styles.TOPLeftBg} >      
+      <Text style={styles.TOPLeft}></Text>
+      </View>
+      <View style={styles.TABLeftBg} >      
+      <Text style={styles.TABLeft}></Text>
+      </View>
+      <View style={styles.midLeftBg} >      
+      <Text style={styles.midLeft}></Text>
+      </View>
+      <View style={styles.midRightBg} >      
+      <Text style={styles.midRight}></Text>
+      </View>
+      <View style={styles.TABRightBg} >      
+      <Text style={styles.TABRight}></Text>
+      </View>
+      <View style={styles.TOPRightBG} >      
+      <Text style={styles.TOPRight}></Text>
+      </View>
+
+      </View>
+      {/* <View style={styles.part2}><Text style={styles.text}>LETS START MYSELF</Text><Text style={styles.text1}>LETS START MYSELF</Text></View>
+      <Button title="Play Sound" onPress={playSound} />  */}
+     <View style={styles.part3}><Text style={styles.text}>Open up App.js to start working on your app!</Text></View> 
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   
   container: {
-    
+    marginTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#0000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
   part1 :{
-    alignItems: "center",
-    justifyContent:"center",
-    flex: 3,
-    backgroundColor: "#199100",
-  paddingTop:"10%",
+    flexDirection: "row",
+    // alignItems: 'center',
+    // justifyContent:"center",
+    flex: 1,
+    // backgroundColor: "#e5090000",
     width: "100%",
   },
-  part2:{
-    paddingTop:"20%",
-    alignItems: "center",
+  part3 :{
+    
+    alignItems: 'center',
     justifyContent:"center",
-    // paddingTop:"25%",
-    flex: 3,
-    backgroundColor: "#e50904",
-
+    flex: 9,
+    // backgroundColor: "#e60900",
+    backgroundColor: "#199100",
     width: "100%",
   },
   text:{
+    backgroundColor: "#e50999",
+
     color: '#cdf104',
     fontSize:20,
     height:"50%",
     width:"100%",
     padding:"10%"
-  }
+  },
+  TOP:{   
+     flex:1,
+     flexDirection:"row",
+  
+  },
+  TOPLeftBg:{
+    backgroundColor: "#e50900",
+    height:"100%",
+    flex:1,
+    
+  },
+  TOPLeft:{
+    borderBottomRightRadius:20,
+    backgroundColor: "#e50999",
+    flex:1,
+  },
+  TABLeftBg:{
+    paddingTop:9,
+    backgroundColor: "#e50999",
+    height:"100%",
+    flex:7,
+  },
+  TABLeft:{
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20,
+    backgroundColor: "#e60900",
+    color: '#cdf104',
+    height:"100%",
+    flex:1,
+    
+  },
+  midLeftBg:{
+    backgroundColor: "#e50900",
+    height:"100%",
+    flex:2,
+    
+  },
+  midLeft:{
+    borderBottomLeftRadius:20,
+    backgroundColor: "#e50999",
+    height:"100%",
+    flex:1,
+  },
+  midRightBg:{
+    backgroundColor: "#199100",
+    height:"100%",
+    flex:2,
+    
+  },
+  midRight:{
+    borderBottomRightRadius:20,
+    backgroundColor: "#e50999",
+
+    height:"100%",
+    flex:1,
+    
+  },
+  TABRightBg:{
+    paddingTop:9, 
+    backgroundColor: "#e50999",
+  
+    height:"100%",
+    flex:7,
+    
+  },
+  TABRight:{
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20,
+    backgroundColor: "#199100",
+    height:"100%",
+    flex:1,
+    
+  },
+  TOPRightBG:{
+    backgroundColor: "#199100",
+    height:"100%",
+    flex:1,
+    
+  },
+  TOPRight:{
+    borderBottomLeftRadius:20,
+    backgroundColor: "#e50999",
+    height:"100%",
+    flex:1,
+    
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
