340d447b73f72059ea5dd3c1d5f10fa107f5a645
vladLevkovets <vl.vladisla@gmail.com>
2022-10-12 18:19:44 +0200

    keybord

--- client-side/App.js
+++ client-side/App.js
@@ -64,14 +64,14 @@ useEffect(() => {
 
   console.log("test")
   return (
-    <View style={[styles.container, {minHeight: Math.round(0.85*vert)}]}>
+    <View style={[styles.container, {minHeight: vert}]}>
       
     {tapped && logged? 
        
-    <View style={styles.container}>
+    <View style={[styles.container,{minHeight: vert}]}>
    
-       <Text style={styles.statusBar}></Text>
-       <SafeAreaView style={styles.top}>
+       <Text style={[styles.statusBar,{minHeight: 0.05*vert}]}></Text>
+       <SafeAreaView style={[styles.top,{minHeight: 0.1*vert}]}>
           
            <TouchableWithoutFeedback >
                <Text style={tab==="left"?styles.tabLeftA:styles.tabLeft} onPress={()=>{setTab("left"); playMusic(Help)}}>My lists</Text>
@@ -177,7 +177,7 @@ backgroundColor:"black",
 
 
   container: {
-    height:"100%",
+   
     width:"100%",
     flex: 1,
     alignItems: 'center',
@@ -199,266 +199,7 @@ backgroundColor:"black",
     height:"10%",
   },
  
-  // left:{
-  //   paddingTop:"3%",
-  //   paddingBottom:"5%",
-  //   alignItems:'center',
-  //   justifyContent: 'center',
-  //   height:"86%",
-  //   width:"100%",
-  //   backgroundColor: "#ff0000",
-  // },
-  // party:{
-  //   marginTop:5,
-  //   flexDirection:'row',
-  //   justifyContent:"space-between",
-  //   width:"100%",
-  //   height:60,
-  //   backgroundColor:"blue",
-  //   borderRadius:30,
-  // },
-  // eventName:{
-  //   marginTop:10,
-  //   paddingTop:0,
-  //   fontSize:20,
-  //   width:"80%",
-  //   color:"#161515",
-  //   paddingLeft:20 ,
-  //   borderRadius:30,
-  //   height:40,
-  //   color:"white"
-  // },
-  // eventProgress:{
-  //   textAlign:'center',
-  //   marginTop:10,
-  //   marginRight:15,
-  //   fontSize:20,
-  //   width:40,
-  //   height:40,
-  //   borderRadius:20,
-  //   color:"white"   
-  //  },
-   singleEvent:{
-    width:"100%",
-    height:"100%"
-   },
-  //  single:{
-  //   paddingTop:40,
-  //   backgroundColor:"#ff0000",
-  //   position:'absolute',
-  //   height:"100%",
-  //   width:"100%",
-  //   zIndex:1,
-  //  },
-  singleTop:{
-   marginLeft:"5%", 
-   width:"90%",
-   height:40,  
-   backgroundColor:"#005bff",
-   borderRadius:20,
-  },
-  singleName:{
-    width:"90%",
-    color:"yellow",
-    paddingLeft:10,
-    paddingTop:5,
-    fontSize:20,
-  },
-  singleText:{
-    width:"90%",
-    fontSize:15,
-  },
-  singleList:{
-    width:"100%",
-    borderWidth:1,
-    height:"85%",
-   backgroundColor:"#ff0099",
-   borderRadius:20,
-   margin:"5%",
-   marginTop:5,
-   marginBottom:10,
-  },
-  delete:{
-    paddingLeft:"5%",
-    width:"30%",
-    height:"60%",
-    backgroundColor:"black",
-    borderRadius:15,
-  },
-  back:{
-    paddingLeft:"5%",
-    width:"30%",
-    height:"60%",
-    backgroundColor:"blue",
-    borderRadius:15,
-  },
-
-  // mid:{
-  //   paddingHorizontal:"3%",
-  //   paddingTop:"3%",
-  //   paddingBottom:"7%",
-  //   alignItems:'center',
-  //   justifyContent: 'center',
-  //   height:577,
-  //   width:"100%",
-  //   backgroundColor: "#005bff",
-  // },
-//   right:{
-//     paddingHorizontal:"3%",
-//     paddingTop:"3%",
-//     paddingBottom:"7%",
-//     alignItems:'center',
-//     justifyContent: 'center',
-//     height:577,
-//     width:"100%",
-//     backgroundColor: '#10ff00',
-//   },
-
-//   text:{
-    
-//     textAlign: 'center',
-//     borderRadius:20,
-//     backgroundColor:'#ff00eb',
-//     color: '#cdf104',
-//     fontSize:30,
-//     height:"97%",
-//     width:"95%"
-//     ,
-//   },
-//   form:{
-//     borderWidth:1,
-//     paddingHorizontal:'1%',
-//     height:"20%",
-//     justifyContent: 'space-evenly',
-//     borderRadius:20,
-//     margin:"2%",
-//    backgroundColor:"#e4ff00"
-//   },
-//   scroll:{
-//     keyboardDismissMode:'none', 
-//     width:"100%",
-//     flex:1,
-//     backgroundColor:"#e4ff00",
-//     borderRadius:20,
-//   },
-//   list:{
-//    borderWidth:1,
-//     height:"70%",
-//    backgroundColor:"#e4ff00",
-//    borderRadius:20,
-//    margin:"2%",
-//    marginTop:0,
-//   },
-//   inputEvent:{
-//     paddingLeft:10 ,
-//     borderRadius:20,
-//     height:"40%",
-//     backgroundColor:"#abb7b9",
-//   },
-//   inputTodo:{
-//     width:"90%",
-//     paddingLeft:10 ,
-//     borderRadius:20,
-//     height:"100%",
-//     placeholder:"Task",
-//     backgroundColor:"#abb7b9",
-//   },
-//   inputBox:{
-//     height:"40%",
-//     flexDirection:"row",
-//     borderRadius:20,
-//     backgroundColor:"#abb7b9"
-//   },
-//   box:{
-//     marginTop:5,
-//     marginHorizontal:3,
-//     flexDirection:"row",
-//     paddingLeft:10 ,
-//     borderRadius:20,
-//     height:40,
-//     backgroundColor:"#ff0909",
-//   },
-
-//   task:{
-//     paddingTop:5,
-//     fontSize:15,
-//     width:"90%",
-//     color:"#161515",
-//     paddingLeft:3 ,
-//     borderRadius:30,
-//     height:40,
-//     backgroundColor:"#ff0909",
-//   },
-   
-//  makeTask:{
-//   textAlign:'center',
-//   marginTop:5,
-//   marginRight:5,
-//   fontSize:20,
-//   width:30,
-//   height:30,
-//   borderRadius:20,
-//   backgroundColor:"green",
-//   color:"white"
-//  },
-//  buttonPic:{
-//   fontSize:15,
-//   width:30,
-//   height:30,
-//   borderRadius:30,
-  
-//  },
-//  makeTaskButton:{
-//   textAlign:'center',
-//   justifyContent:'flex-start',
-//   margin:5,
-//   fontSize:15,
-//   width:20,
-//   height:20,
-//   borderRadius:0,
-//   backgroundColor:"blue",
-//   color:"white"
-//  },
-//  delTask:{
-//   textAlign:'center',
-//   marginTop:5,
-//   marginRight:5,
-//   fontSize:20,
-//   width:30,
-//   height:30,
-//   borderRadius:20,
-//   backgroundColor:"black",
-//   color:"white"
-// },
-// listBtns:{
-//   justifyContent:"space-around",
-//   flexDirection:"row",
-//   height:40,
-//   width:"100%"
-// },
-// cancel:{
-//   paddingLeft:"5%",
-//   width:"30%",
-//   height:"60%",
-//   backgroundColor:"red",
-//   borderRadius:15,
-// },
-// create:{
-//   paddingLeft:"5%",
-//   width:"30%",
-//   height:"60%",
-//   backgroundColor:"green",
-//   borderRadius:15,
-// },
-// btnsText:{
-//   width:"80%",
-//   height:"100%",
-//   textAlign:'center',
-//   fontSize:15,
-//   color:"yellow",
-//   paddingBottom:5,
-  
-// },
+ 
   tabLeft:{
     paddingTop:9,
     color:"#005bff",
